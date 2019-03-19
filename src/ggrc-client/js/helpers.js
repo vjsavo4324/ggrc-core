e605be8daa410f384b4715e04dbf338fb7236791
YuriChupyrkin <Yuri.Chupyrkin@gmail.com>
2019-03-19 14:09:31 +0300

    Add 'canData' helper

--- src/ggrc-client/js/helpers.js
+++ src/ggrc-client/js/helpers.js
@@ -980,3 +980,13 @@ can.stache.registerHelper('isValidAttrProperty',
       options.inverse(options.contexts);
   }
 );
+
+can.stache.registerHelper('canData',
+  (key, options) => {
+    key = resolve(key);
+
+    return function (el) {
+      $(el).data(key, options.context);
+    };
+  }
+);

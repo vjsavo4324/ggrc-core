b999ea3c8b880708ba8c0289c36c2224f1b9704d
YuriChupyrkin <Yuri.Chupyrkin@gmail.com>
2019-03-19 13:13:09 +0300

    Update renderLive helper

--- src/ggrc-client/js/helpers.js
+++ src/ggrc-client/js/helpers.js
@@ -202,7 +202,8 @@ can.stache.registerHelper('renderLive', function (template, context, options) {
     options.contexts = options.contexts.add(options.hash);
   }
 
-  return can.view.render(template, options.contexts);
+  let view = GGRC.Templates[template];
+  return can.stache(view)(options.contexts);
 });
 
 function deferRender(tagPrefix, funcs, deferred) {
